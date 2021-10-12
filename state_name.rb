3327eb8515dbcc2969255267e43d1fc9621f0be1
luciane <lucianefernandescdf@gmail.com>
2021-10-11 22:22:22 -0300

    add metod get state

--- state_name.rb
+++ state_name.rb
@@ -1,6 +1,17 @@
 require 'faraday'
+attr_reader :state
 class StateName
-  def get#comportamento
-    Faraday.get'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+
+  def initialize
+    @state = states #substantivos
+  end
+  
+  def get_state(url)
+    @state = state.get(url)
+  end
+
+  private
+  def url#comportamento
+    'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
   end
 end
