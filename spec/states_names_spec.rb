4ef4279daa884952e05c1660be9e908ebe36b361
luciane <lucianefernandescdf@gmail.com>
2021-12-06 18:11:29 -0300

    wip context

--- spec/states_names_spec.rb
+++ spec/states_names_spec.rb
@@ -2,11 +2,15 @@ require 'faraday'
 require_relative '../states_names'
 
 describe StatesNames do
-  context '.call' do
-    let(:states_names) { StatesNames.new }
-    it 'retorna o faraday', :vcr do
-      estado = states_names.parse
-      expect(estado.length).to eq(27)
+  context '.parse' do
+    context 'when the api return states' do
+      let(:states_names) { StatesNames.new }
+      it 'retorna o faraday', :vcr do
+        estado = states_names.parse
+        expect(estado.length).to eq(27)
+        expect(estado.first).to eq([11, 'Rondônia', 'RO'])
+        expect(estado.last).to eq([53, 'Distrito Federal', 'DF'])
+      end
     end
   end
 end
