2f808495f47a96853841b67a1b632e1792dda819
luciane <lucianefernandescdf@gmail.com>
2022-01-05 19:15:42 -0300

    removed get

--- states_names.rb
+++ states_names.rb
@@ -1,15 +1,23 @@
+require 'faraday'
 class StatesNames
-  attr_reader :states
+  attr_reader :states, :states_parse, :response, :get
 
   def initialize(states)
     @states = states
   end
 
   def get
-    @states = states.get(url)
+    @states = states.get(response)
   end
 
-  def url # comportamento
+  def states_parse
+    json = JSON.parse { response.body symbolize_names: true }
+    response = json.map do |state|
+      states = state[:id], state[:sigla], state[:nome]
+    end
+  end
+
+  def response
     'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
   end
 end
